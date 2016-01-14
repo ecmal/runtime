828c69fc9e2c0b75cc6377e70d72ae66c1d96adc
Sergey Mamyan <sergey.mamyan@gmail.com>
2016-01-14 23:22:29 +0400

    Publish Script Changes

--- publish.sh
+++ publish.sh
@@ -3,8 +3,13 @@ rm -rf out
 
 git worktree prune
 git worktree add ./out/runtime release
+cd ./out/runtime
+pwd
 git rm -r .
 ./compile.sh
 git add .
-git commit
+git commit -am 'Publish Changes';
 git push -u origin
+cd ../../
+rm -rf out
+
