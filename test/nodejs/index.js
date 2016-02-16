1c65d1aabe6bb0807dcc7f1589ef70a1208f8a17
Sergey Mamyan <sergey.mamyan@gmail.com>
2016-02-16 21:17:49 +0400

    Refactoring Reflection API

--- test/nodejs/index.js
+++ test/nodejs/index.js
@@ -1,8 +1,5 @@
 require('./out/runtime/package');
-System.import('nodejs/main')
-    .then(function(r){
-        console.info(new r.Main("JAN"));
-    })
+System.import('nodejs/index')
     .catch(function(e){
         console.error(e.stack)
     })
