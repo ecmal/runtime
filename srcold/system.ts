dae55693dc3857fb65a7602d7aba64d0b4f22247
Sergey Mamyan <sergey.mamyan@gmail.com>
2016-03-07 18:37:28 +0400

    Move to TypeScript 1.8

---
+++ srcold/system.ts
@@ -0,0 +1,43 @@
+///<reference path="./helpers.ts"/>
+///<reference path="./reflect.ts"/>
+///<reference path="./runtime/loader.ts"/>
+///<reference path="./runtime/browser.ts"/>
+///<reference path="./runtime/node.ts"/>
+
+class System {
+    constructor(){
+        if(!Runtime.Loader.global.System){
+            Runtime.Loader.global.System = System;
+        }
+        return System;
+    }
+    static get platform():string{
+        return Runtime.Loader.platform;
+    }
+    static get loader() : Runtime.Loader {
+        return Object.defineProperty(this,'loader',<any>{
+            value:(():Runtime.Loader=>{
+                switch(System.platform){
+                    case 'browser'  : return new Runtime.BrowserLoader();
+                    case 'node'     : return new Runtime.NodeLoader();
+                }
+            })()
+        }).loader;
+    }
+    static get modules(): Reflect.Modules {
+        return Reflect.MODULES;
+    }
+    static module(uri:string):Promise<Reflect.Module>{
+        return this.loader.module(uri);
+    }
+    static import(uri:string):Promise<any>{
+        return this.loader.import(uri);
+    }
+    static register(requires:string[],execute:Function):void{
+        this.loader.register(requires,execute);
+    }
+    static bundle(content){
+        this.loader.bundle(content);
+    }
+}
+new System();
